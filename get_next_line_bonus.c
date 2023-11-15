1b0c300a40f340269e7e19fbc8c1c3eb7fe8252d
PierreLgol <plgol.perso@gmail.com>
2023-11-15 11:31:45 +0100

    get_next_line stupid version

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: plgol.perso <pollivie@student.42.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/14 21:07:07 by plgol.perso       #+#    #+#             */
-/*   Updated: 2023/11/14 21:07:08 by plgol.perso      ###   ########.fr       */
+/*   Created: 2023/11/15 11:31:14 by plgol.perso       #+#    #+#             */
+/*   Updated: 2023/11/15 11:31:15 by plgol.perso      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@
 
 char	*get_next_line(int fd)
 {
-	static char	*prev;
+	static char	*prev[PSIZE];
 	char		*curr;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	prev = gnl_advance(fd, prev);
-	if (!prev)
+	prev[(fd * 128) % PSIZE] = gnl_advance(fd, prev[(fd * 128) % PSIZE]);
+	if (!prev[(fd * 128) % PSIZE])
 		return (NULL);
-	curr = gnl_produce(prev);
-	prev = gnl_consume(prev);
+	curr = gnl_produce(prev[(fd * 128) % PSIZE]);
+	prev[(fd * 128) % PSIZE] = gnl_consume(prev[(fd * 128) % PSIZE]);
 	return (curr);
 }
 
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,27 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: plgol.perso <pollivie@student.42.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/14 21:07:45 by plgol.perso       #+#    #+#             */
-/*   Updated: 2023/11/14 21:07:46 by plgol.perso      ###   ########.fr       */
+/*   Created: 2023/11/15 11:31:36 by plgol.perso       #+#    #+#             */
+/*   Updated: 2023/11/15 11:31:37 by plgol.perso      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <stddef.h>
 # include <stdlib.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1000
-# endif
-
-# ifndef PSIZE
-#  define PSIZE 2048
 # endif
 
 size_t	gnl_strlen(char *str);
